238af6a9f8003b7191bba0e26f2711e06756b090
Karel Lenc <lenckarel@gmail.com>
2016-07-21 15:00:32 +0100

    Skip when magnification is one.

--- matlab/+utls/frame_magnify_scale.m
+++ matlab/+utls/frame_magnify_scale.m
@@ -1,6 +1,8 @@
 function [ frames ] = frame_magnify_scale( frames, mf )
 
-frames = utls.frame_set_scale(frames, utls.frame_get_scale(frames) * mf);
+if mf ~= 1
+  frames = utls.frame_set_scale(frames, utls.frame_get_scale(frames) * mf);
+end
 
 end
 
