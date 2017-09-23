f8a7acaaa22702127331175b69c48febc5ba39e9
Xu Zhang <spongezhang@gmail.com>
2017-09-23 00:11:46 -0400

    bug fix in ellipse_overlap_H. Valid check may remove all features.

--- matlab/+utls/covdet_preprocessim.m
+++ matlab/+utls/covdet_preprocessim.m
@@ -5,5 +5,9 @@ if isa(img, 'uint8'), img = im2single(img); end;
 if isa(img, 'double'), img = single(img); end;
 if size(img, 3) == 3, img = rgb2gray(img); end;
 
+%We may needs to tell people that image value are change to 0-1.
+%may only work for vlfeat detector and may not work for other detectors
+%img = img*255;
+
 end
 
