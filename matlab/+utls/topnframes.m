73bdd43588d439b5ed8296eda1925cf61a71f0f6
Karel Lenc <lenckarel@gmail.com>
2017-11-15 17:38:56 +0000

    Quick exit if topn=inf.

--- matlab/+utls/topnframes.m
+++ matlab/+utls/topnframes.m
@@ -2,6 +2,7 @@ function [ feats ] = topnframes( feats, n )
 %TOPNFEATS pick only top-N features based on the detresponses
 %   Detailed explanation goes here
 assert(isfield(feats, 'detresponses'));
+if isinf(n), return; end;
 [~, perm] = sort(feats.detresponses, 'descend');
 perm = perm(1:min(n, numel(perm)));
 feats.frames = feats.frames(:, perm);
