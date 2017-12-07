cbc2da3857765fffe731d966c579ad1112bcb672
Karel Lenc <lenckarel@gmail.com>
2017-12-07 22:16:25 +0000

    Separate script for running system commands.

--- matlab/+features/+det/tcdet.m
+++ matlab/+features/+det/tcdet.m
@@ -10,9 +10,7 @@ opts.runDir = fullfile(opts.binDir, 'tensorflow');
 [opts, varargin] = vl_argparse(opts, varargin);
 opts.point_number = 4000;
 opts.thr = 1.2;
-opts.gpu = [];
-opts.unset_ld = false; % Set true if segfaults, has to be false for CUDA
-opts = vl_argparse(opts, varargin);
+[opts, varargin] = vl_argparse(opts, varargin);
 
 res.detName = 'tcdet'; res.args = opts; res.frames = zeros(5, 0);
 if isempty(img), return; end;
@@ -35,23 +33,21 @@ scriptPath = fullfile(vlb_path, 'matlab', '+features', '+utls', 'tcdet_eval.py')
 copyfile(scriptPath, opts.runDir);
 scriptPath = fullfile(vlb_path, 'matlab', '+features', '+utls', 'tcdet_rundet.m');
 copyfile(scriptPath, opts.runDir);
-if iscell(opts.gpu), opts.gpu = strjoin(opts.gpu, ','); end
-addcmd = '';
-if opts.unset_ld, addcmd = '--unset=LD_LIBRARY_PATH '; end;
-cmd = sprintf('env %s CUDA_DEVICE_ORDER="PCI_BUS_ID" CUDA_VISIBLE_DEVICES=%s python tcdet_eval.py "%s" --save_feature "%s"', addcmd, opts.gpu, imname, featsname);
+
+cmd = sprintf('python tcdet_eval.py "%s" --save_feature "%s"', imname, featsname);
+utls.sysrun(cmd, 'runDir', opts.runDir, varargin{:});
+
 actpath = pwd;
 try
   cd(opts.runDir);
-  [ret, out] = system(cmd, '-echo');
   [res.frames, res.detresponses] = tcdet_rundet(img, featsname, ...
     opts.point_number, opts.thr);
-catch 
+catch e
   cd(actpath);
+  throw(e);
 end
 cd(actpath);
-if ret ~= 0
-  error('Error running TCDET python script.\n%s', out);
-end
+
 res.frames(1:2,:) = bsxfun(@minus, res.frames(1:2,:), padding');
 res.frames(:, res.frames(1,:) < 0 | res.frames(2,:) < 0) = [];
 res.frames(:, res.frames(1,:) > imsz(2) | res.frames(2,:) > imsz(1)) = [];
---
+++ matlab/+utls/sysrun.m
@@ -0,0 +1,39 @@
+function [ out ] = sysrun( cmd, varargin )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+opts.gpu = [];
+[opts, varargin] = vl_argparse(opts, varargin);
+opts.unset_ld = isempty(opts.gpu); % Set true if segfaults, has to be false for CUDA
+opts.runDir = pwd();
+opts.verbose = false;
+opts = vl_argparse(opts, varargin);
+
+if ~isempty(opts.gpu)
+  opts.gpu = cellfun(@num2str, opts.gpu - 1, 'Uni', false);
+  opts.gpu = strjoin(opts.gpu, ',');
+end
+addcmd = '';
+if opts.unset_ld, addcmd = '--unset=LD_LIBRARY_PATH '; end
+fullcmd = sprintf('env %s CUDA_DEVICE_ORDER="PCI_BUS_ID" CUDA_VISIBLE_DEVICES=%s %s', ...
+  addcmd, opts.gpu, cmd);
+
+actpath = pwd;
+try
+  cd(opts.runDir);
+  if opts.verbose
+    fprintf('Running:\n%s\n', fullcmd);
+    [ret, out] = system(fullcmd, '-echo');
+  else
+    [ret, out] = system(fullcmd);
+  end
+catch e
+  cd(actpath);
+  throw(e);
+end
+cd(actpath);
+if ret ~= 0
+  error('Error running %s.\n%s', fullcmd, out);
+end
+
+end
+
