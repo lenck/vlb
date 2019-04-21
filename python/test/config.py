d9be269bc1fe050134b44521a295f1a45ad8ee96
rbutenko3 <albutko@gatech.edu>
2019-04-20 22:17:01 -0400

    create config file

---
+++ python/test/config.py
@@ -0,0 +1,56 @@
+import os
+import sys
+cwd = os.getcwd()
+sys.path.insert(0, '{}/python/'.format(cwd))
+
+import features.cyvlsift_official
+import features.cv_orb
+import features.cv_mser
+import features.cv_brisk
+import features.cv_fast
+import features.cv_akaze
+import features.cv_kaze
+import features.superpoint
+import features.lf_net
+from importlib import import_module
+
+
+
+what_models_to_test = {
+    'superpoint': {
+        'class':'SuperPoint',
+        'test':False
+        },
+    'cyvlsift_official':{
+        'class':'cyvlsift_official',
+        'test':False},
+    'cv_orb':{
+        'class':'cv_orb',
+        'test':True},
+    'cv_mser':{
+        'class':'cv_mser',
+        'test':True},
+    'cv_brisk':{
+        'class':'cv_brisk',
+        'test':False},
+    'cv_fast':{
+        'class':'cv_fast',
+        'test':False},
+    'cv_akaze':{
+        'class':'cv_akaze',
+        'test':False},
+    'cv_kaze':  {
+        'class':'cv_kaze',
+        'test':False},
+    'lf_net':  {
+        'class':'LFNet',
+        'test':False}}
+
+
+models_to_test = list()
+
+for model, conf in what_models_to_test.items():
+    if conf['test']:
+        mod = import_module(f"features.{model}")
+        cl = getattr(mod, conf['class'])
+        models_to_test.append((model, cl()))
--- python/test/test_ms_bench.py
+++ python/test/test_ms_bench.py
@@ -36,47 +36,20 @@ import features.superpoint
 import dset.vgg_dataset
 import pickle as pkl
 
+from config import models_to_test
 
 if __name__ == "__main__":
 
     # Define matching score benchmark
     ms_bench = bench.MatchingScoreBench.MatchingScoreBench()
 
-    # Define features
-    vlsift_py = features.cyvlsift_official.cyvlsift_official()
-    cv_orb = features.cv_orb.cv_orb()
-    cv_brisk = features.cv_brisk.cv_brisk()
-    cv_fast = features.cv_fast.cv_fast()
-    cv_akaze = features.cv_akaze.cv_akaze()
-    cv_kaze = features.cv_kaze.cv_kaze()
-    superpoint = features.superpoint.SuperPoint()
-
     # Define dataset
     vggh = dset.vgg_dataset.vggh_Dataset()
 
-    # Do the evaluation
-    ms_result_vlsift = ms_bench.evaluate(
-        vggh, vlsift_py, use_cache=True, save_result=True)
-
-    ms_result_cv_orb = ms_bench.evaluate(
-        vggh, cv_orb, use_cache=True, save_result=True)
-
-    ms_result_cv_brisk = ms_bench.evaluate(
-        vggh, cv_brisk, use_cache=True, save_result=True)
-
-    ms_result_cv_kaze = ms_bench.evaluate(
-        vggh, cv_kaze, use_cache=True, save_result=True)
-
-    ms_result_cv_akaze = ms_bench.evaluate(
-        vggh, cv_akaze, use_cache=True, save_result=True)
-
-    # ms_result_superpoint = ms_bench.evaluate(
-    #     vggh, superpoint, use_cache=True, save_result=True)
-
-    # Make the results from different detectors as a list.
-    ms_result = [ms_result_vlsift, ms_result_cv_orb,
-                 ms_result_cv_brisk, ms_result_cv_kaze,
-                 ms_result_cv_akaze]
+    ms_result = list()
+    for (modelName, model) in models_to_test:
+        vgg_result = ms_bench.evaluate(vggh, model, use_cache=True, save_result=True)
+        ms_result.append(vgg_result)
 
     # ms_result = [ms_result_superpoint]
 
