db2d56da6e084701c99344cf6f80e2f3b10255ba
rbutenko3 <albutko@gatech.edu>
2019-04-23 11:45:00 -0400

    create weight download script

---
+++ python/features/download_weights.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#LF-Net Pretrained Models
+curl http://webhome.cs.uvic.ca/~kyi/files/2018/lf-net/pretrained.tar.gz | tar xz -C ./lf_net_misc/
+
+#TF-Lift models
+curl http://webhome.cs.uvic.ca/~kyi/files/2018/tflift/release-aug.tar.gz | tar xz -C ./tf-lift/
+
+curl https://github.com/MagicLeapResearch/SuperPointPretrainedNetwork/raw/master/superpoint_v1.pth | tar xv -C ./superpoint_misc/
+
+# Download HardNet pretrained model
+#cd ./hardnet && { curl -O http://cs.unc.edu/~xufeng/matchnet/models/liberty_r_0.01_m_0.feature_net.pb; cd -;}
