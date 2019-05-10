3572740edb42bdf14980025d682a2e4f5fce2b6b
rbutenko3 <albutko@gatech.edu>
2019-05-10 16:25:26 -0400

    create geometric verification algo template

---
+++ python/verifiers/VerificationTemplate.py
@@ -0,0 +1,32 @@
+
+
+
+class VerificationTemplate():
+    """Basic Template for Verification Algorithm
+
+    Attributes
+    ----------
+
+    name: str
+        Name of the method
+    estimates_essential: boolean, optional
+        Does the algorithm estimate the essential matrix
+    estimates_fundamental: boolean, optional
+        Does the algorithm estimate the fundamental matrix
+
+    """
+
+    def __init__(self, name, estimates_essential=False, estimates_fundamental=False):
+
+        self.name = name
+        self.estimates_essential = estimates_essential
+        self.estimates_fundamental = estimates_fundamental
+
+    def evaluate_matches(self, pts1, pts2):
+        pass
+
+    def estimate_essential_matrix(self, pts1, pts2):
+        pass
+
+    def estimate_fundamental_matrix(self, pts1, pts2):
+        pass
