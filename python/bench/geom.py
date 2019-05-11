fix calculation for symmetrix distanct

python/bench/geom.py
@@ -202,19 +202,32 @@ def get_episym_w_matrix(x1, x2, M):
         x2, np.ones((num_pts, 1))
     ], axis=-1).reshape(-1, 3, 1)
 
-
+    Mt = np.repeat(M.T.reshape(-1, 3, 3), num_pts, axis=0)
     M = np.repeat(M.reshape(-1, 3, 3), num_pts, axis=0)
-
+    
     x2Mx1 = np.matmul(x2.transpose(0, 2, 1), np.matmul(M, x1)).flatten()
     Mx1 = np.matmul(M, x1).reshape(-1, 3)
-    Mtx2 = np.matmul(M.transpose(0, 2, 1), x2).reshape(-1, 3)
+    Mtx2 = np.matmul(Mt, x2).reshape(-1, 3)
 
-    ys = x2Mx1**2 * (
-        1.0 / (Mx1[..., 0]**2 + Mx1[..., 1]**2) +
-        1.0 / (Mtx2[..., 0]**2 + Mtx2[..., 1]**2))
+    x1Mtx2 = np.matmul(x1.transpose(0, 2, 1), np.matmul(Mt, x2)).flatten()
+
+
+
+    ys = x2Mx1 * (
+        1.0 / np.sqrt(Mx1[..., 0]**2 + Mx1[..., 1]**2)) + x1Mtx2 * (
+        1.0 / np.sqrt(Mtx2[..., 0]**2 + Mtx2[..., 1]**2))
 
     return ys.flatten()
 
+def get_epidist(x1,x2, M, type):
+    if type =='symmetric':
+        return get_episym_w_matrix(x1, x2, M)
+    elif type == 'vanilla':
+        return get_epidist_w_matrix(x1, x2, M)
+
+    else:
+        print('{} not supported'.format(type))
+        return
 
 def get_sampsons_w_matrix(x1, x2, M):
 
@@ -250,7 +263,12 @@ def get_E_matrix(dR, dt):
 def get_F_matrix(dR, dt, K1, K2):
     E = get_E_matrix(dR, dt)
     F = np.matmul(np.linalg.inv(K2).T, np.matmul(E,np.linalg.inv(K1)))
-    
+
+    return F
+
+def get_F_matrix_from_E(E, K1, K2):
+    F = np.matmul(np.linalg.inv(K2).T, np.matmul(E,np.linalg.inv(K1)))
+
     return F
 
 def to_pixel_coords(K, pts):
@@ -265,12 +283,20 @@ def to_pixel_coords(K, pts):
     proj_pts = np.matmul(K, pts.T)[:-1,:]
     return proj_pts.T
 
-def get_inliers_F(x1, x2, F, thresh=1):
-    epi_dist = get_episym_w_matrix(x1, x2, F)
+def get_inliers_F(x1, x2, F, dist_type='symmetric', thresh=1):
+    if dist_type == 'vanilla':
+        epi_dist = get_epidist_w_matrix(x1, x2, F)
+    elif dist_type == 'symmetric':
+        epi_dist = get_episym_w_matrix(x1, x2, F)/float(2)
+    else:
+        print('{} is not a supported distance type'.format(dist_type))
+        return
+    inlier_mask = np.zeros(x1.shape[0])
+    inlier_mask[epi_dist < thresh] = 1
 
     x1 = x1[epi_dist < thresh,:]
     x2 = x2[epi_dist < thresh,:]
-    return x1, x2
+    return x1, x2, inlier_mask
 
 
 def get_epi_constraint(x1, x2, F):